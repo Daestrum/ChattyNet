880a96069b2cf7fc58008b5a5f110f1f973e5276
Gary <you@users.noreply.github.com>
2026-05-15 23:33:55 +0100

    built new template

---
+++ ToolTemplate/ToolTemplate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolTemplate
+{
+    public class ToolTemplate
+    {
+        public string Name => "tool_name";  // unique name for the tool, e.g. "get_date"
+        public string Description => "Returns ??";  // one line description of what the tool does  
+        public string Schema => "{}";  // JSON schema for input parameters, e.g. { "type": "object", "properties": { "param1": { "type": "string" } }, "required": ["param1"] }
+        public string Type => "output";  // input, output, action 
+        public string CanUse => "free";  // restricted, paid, free
+        public bool Tool => false; // is this a tool that can be called by the AI, or just a function for internal use by other tools
+        public string Run(string jsonInput)  // jsonInput will be a JSON string that matches the Schema defined above
+        {
+            // what tool does goes here
+            // return response to AI (JSON format)
+            return "{}";
+        }
+    }    
+}
--- ToolTemplate/obj/Debug/net10.0/ToolTemplate.AssemblyInfo.cs
+++ ToolTemplate/obj/Debug/net10.0/ToolTemplate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToolTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+200c730d24415ec0bcc0096926462011c2eede5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31cbadf4d6c02808074c622d5f76d0150599fb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToolTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToolTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
