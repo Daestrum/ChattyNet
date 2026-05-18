20e14024ae1ded4775961fe1d865c9c71d6e59ca
Gary <you@users.noreply.github.com>
2026-05-18 18:50:31 +0100

    tidy up

---
+++ dummy_tool/DummyTool.cs
@@ -0,0 +1,16 @@
+﻿namespace DummyTool
+{
+    public class DummyTool
+    {
+        public string Name => "dummy_tool";
+        public string Description => "Just a dummy tool for testing";
+        public string Schema => "{}";
+        public string Type => "output";
+        public string CanUse => "free";
+        public bool Tool => true;
+        public string Run(string args)
+        {
+            return $"{{ \"dummy_result\": \"dummy_value\" }}";
+        }
+    }
+}
